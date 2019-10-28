0451b9468a7fee3787b5d34f32e3f82e124d3110
zhujian <505169307@qq.com>
2019-10-28 15:57:45 +0800

    perf(script): 配置GIT和SSH运行环境

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,30 @@
 set -eux
 
 GIT_DIR="upload_git"
-USER_NAME="zhujian"
-USER_EMAIL="505169307@qq.com"
+USER_NAME=`git config --global user.name`
+USER_EMAIL=`git config --global user.email`
+SSH_CONFIG=`cat ~/.ssh/config | grep ${HEXO_SERVER_ADDRESS}`
+
+# GIT环境
+
+if [[ -z ${GIT_USER_NAME} ]]
+then
+	git config --global user.name ${GIT_USER_NAME}
+fi
+
+if [[ -z ${USER_EMAIL} ]]
+then
+    git config --global user.email ${GIT_USER_EMAIL}
+fi
+
+## SSH环境
+
+if [[ -z ${SSH_CONFIG} ]]
+then
+    echo -e "Host ${HEXO_SERVER_ADDRESS}\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+fi
+
+## GIT操作
 
 cd ./blogs/
 
@@ -15,13 +37,10 @@ fi
 mkdir ${GIT_DIR}
 cd ${GIT_DIR}
 
-git config --global user.name ${USER_NAME}
-git config --global user.email ${USER_EMAIL}
-
 git init
 cp -r ../public/* ./
 git add .
 git commit -m "Update blogs"
-git push --force git@148.70.133.9:/data/repositories/blogs.git master
+git push --force git@${HEXO_SERVER_ADDRESS}:/data/repositories/blogs.git master
 git push --force git@github.com:zjZSTU/zjzstu.github.io.git master
 git push --force git@git.dev.tencent.com:zjZSTU/zjZSTU.coding.me.git master
