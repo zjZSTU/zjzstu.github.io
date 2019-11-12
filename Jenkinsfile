78f4fa826137582fb149a5e80575d85d7cf23613
zhujian <505169307@qq.com>
2019-11-12 21:08:52 +0800

    ci(jenkins): 测试

--- Jenkinsfile
+++ Jenkinsfile
@@ -8,17 +8,17 @@ pipeline {
                 sh 'scripts/install.sh'
             }
         }
-        stage('Build') {
-            steps {
-                echo 'Building..'
-                sh 'scripts/build.sh'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-                sh 'scripts/deploy.sh'
-            }
-        }
+        // stage('Build') {
+        //     steps {
+        //         echo 'Building..'
+        //         sh 'scripts/build.sh'
+        //     }
+        // }
+        // stage('Deploy') {
+        //     steps {
+        //         echo 'Deploying....'
+        //         sh 'scripts/deploy.sh'
+        //     }
+        // }
     }
 }
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,15 +3,20 @@
 set -eux
 cd ./blogs/
 
+git branch -vv
+git branch -a
+
 NEXT_DIR="themes/next"
 CANVAS_DIR="${NEXT_DIR}/source/lib/canvas-nest"
 ALGOLIA_DIR="${NEXT_DIR}/source/lib/algolia-instant-search"
 FANCYBOX_DIR="${NEXT_DIR}/source/lib/fancybox"
+PACE_DIR="${NEXT_DIR}/source/lib/pace"
 
-NEXT_GIT="http://localhost/gitlabs/zjzstu/hexo-theme-next.git"
-CANVAS_GIT="http://localhost/gitlabs/zjzstu/theme-next-canvas-nest.git"
-ALGOLIA_GIT="http://localhost/gitlabs/zjzstu/theme-next-algolia-instant-search.git"
-FANCYBOX_GIT="http://localhost/gitlabs/zjzstu/theme-next-fancybox3.git"
+NEXT_GIT="http://192.168.0.144:7010/zjZSTU/hexo-theme-next.git"
+CANVAS_GIT="http://192.168.0.144:7010/zjZSTU/theme-next-canvas-nest.git"
+ALGOLIA_GIT="http://192.168.0.144:7010/zjZSTU/theme-next-algolia-instant-search.git"
+FANCYBOX_GIT="http://192.168.0.144:7010/zjZSTU/theme-next-fancybox3.git"
+PACE_GIT="http://192.168.0.144:7010/zjZSTU/theme-next-pace.git"
 
 function clone()
 {
@@ -24,9 +29,31 @@ function clone()
     git clone ${GIT} ${DIR}
 }
 
-clone ${NEXT_DIR} ${NEXT_GIT}
+function next() 
+{
+    DIR=$1
+    GIT=$2
+
+    if [ -e ${DIR} ]
+    then
+        rm -rf ${DIR}
+    fi
+    mkdir ${DIR}
+    cd ${DIR}
+    git init
+    git remote add origin ${GIT}
+    git fetch origin dev
+    git checkout -b dev origin/dev
+    
+    git branch -vv
+    git branch -a
+    cd ../..
+}
+
+next ${NEXT_DIR} ${NEXT_GIT}
 clone ${CANVAS_DIR} ${CANVAS_GIT}
 clone ${ALGOLIA_DIR} ${ALGOLIA_GIT}
 clone ${FANCYBOX_DIR} ${FANCYBOX_GIT}
+clone ${PACE_DIR} ${PACE_GIT}
 
 npm install
