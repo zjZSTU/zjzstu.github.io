06ff60a4e4153af1c125b5d28ba727164f6e7563
zhujian <505169307@qq.com>
2019-12-05 12:17:57 +0800

    feat(search): 使用local_search服务替代aloglia search

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,8 @@
 
 set -eux
 
-# algolia环境
-export HEXO_ALGOLIA_INDEXING_KEY=${HEXO_ALGOLIA_KEY}
-
 cd ./blogs/
 rm node_modules/kramed/lib/rules/inline.js
 cp inline.js node_modules/kramed/lib/rules/
 
 npm run cg
-hexo algolia
