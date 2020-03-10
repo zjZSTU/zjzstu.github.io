b8b28527a6992309694645cf8b6cfc16ebff8f69
zhujian <505169307@qq.com>
2020-03-10 19:12:09 +0800

    ci(jenkins): 取消ssh端口设置

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,9 @@ set -eux
 function set_config
 {
     echo -e "Host ${HEXO_SERVER_ADDRESS}\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-    echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tPort 443\n" >> ~/.ssh/config
-    echo -e "Host e.coding.net\n\tStrictHostKeyChecking no\n\tPort 443\n" >> ~/.ssh/config
+    echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+    echo -e "Host coding.net\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+    chmod 600 ~/.ssh/config
 }
 
 # GIT环境
